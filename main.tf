46d2dd5ecbd4cf243022cb98f327090e42a69373
mberglof <marcus.berglof@gmail.com>
2025-03-28 17:57:12 +0000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,95 @@
+terraform {
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+    }
+  }
+}
+
+provider "libvirt" {
+  uri = "qemu:///system"
+}
+
+resource "libvirt_volume" "volumes" {
+  name    = "tf-jammy.${var.img_format}"
+  pool    = "default"
+  source  = var.img_source
+  format  = var.img_format
+}
+
+resource "libvirt_domain" "guest" {
+  name   = "tf-jammy"
+  memory = var.mem_size
+  vcpu   = var.cpu_cores
+
+  disk {
+    volume_id = libvirt_volume.volumes.id
+  }
+
+  # disk {
+  #   file = "${path.module}/image.iso"
+  # }
+
+  cloudinit = libvirt_cloudinit_disk.commoninit.id
+
+  network_interface {
+    network_name = "default"
+    wait_for_lease = true
+  }
+
+  console {
+    type        = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+
+  graphics {
+    type      = "spice"
+    autoport  = true
+    listen_type = "address"
+  }
+}
+
+resource "libvirt_cloudinit_disk" "commoninit" {
+  name      = "commoninit.iso"
+  user_data = data.template_file.user_data.rendered
+}
+
+data "template_file" "user_data_yaml" {
+    template = yamlencode({
+    users = [
+      {
+        name = "k8s_user"
+        gecos = "k8s_user"
+        shell = "/bin/bash"
+        primary_group = "k8s"
+        groups = "users"
+        sudo = "ALL=(ALL) NOPASSWD:ALL"
+        lock_passwd = false 
+        ssh_authorized_keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDX2gcunVOrLn5ULiht/FJo4xHjslWubSG7K5b5YFaF6",
+        ]
+      },
+    ]
+    package_update = true
+    package_upgrade = true
+    packages = [
+      "ca-certificates",
+      "curl",
+    ]
+    runcmd = [
+      "curl -LsSf https://get.k3s.io | sh -",
+    ]
+  })
+}
+
+data "template_file" "user_data" {
+  template = file("${path.module}/${var.cloud_init}.cfg")
+}
+
+output "user_data" {
+  value = data.template_file.user_data_yaml
+}
+output "ips" {
+  value = libvirt_domain.guest.*.network_interface.0.addresses
+}
---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "cloud_init" {
+    description = "Cloud init config file"
+    default = "cloud_init"
+    type = string
+}
+
+variable "img_source" {
+  description = "Ubuntu 22.04 LTS Cloud Image"
+  default = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+}
+
+variable "img_format" {
+  description = "QCow2 UEFI/GPT Bootable disk image"
+  default = "qcow2"
+  type = string
+}
+
+variable "mem_size" {
+  description = "Amount of RAM (in MiB) for the virtual machine"
+  type        = string
+  default     = "2048"
+}
+
+variable "cpu_cores" {
+  description = "Number of CPU cores for the virtual machine"
+  type        = number
+  default     = 2
+}
